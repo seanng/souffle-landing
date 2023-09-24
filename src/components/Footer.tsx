d70aaad2488c83f886235bc5e94ef9990dc69dbe
sean <shonum@gmail.com>
2023-09-24 15:44:31 +0800

    chore: fix build issue

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ export function Footer() {
     <Container as="footer" className="w-full">
       <div className="mb-20 mt-24 flex flex-wrap items-end justify-between gap-x-6 gap-y-4 border-t border-neutral-950/10 pt-12">
         <Link href="/" aria-label="Home">
-          <Logo className="h-8" fillOnHover />
+          <Logo className="h-8" />
         </Link>
         <p className="text-sm text-neutral-700">
           © ChatSouffle {new Date().getFullYear()}
